00b87d0087faad6c5f9541b37c30b863bced4eba
Luke <lukeduffy84@gmail.com>
2021-11-11 13:39:25 +0000

    updated static root

--- fitness/settings.py
+++ fitness/settings.py
@@ -117,11 +117,10 @@ LOGOUT_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [str(BASE_DIR / "static")]
-
-print(STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
